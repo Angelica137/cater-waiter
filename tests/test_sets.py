12a15be80e8119a3b4fb77d3dc8fd77b80f7bd7a
Angelica <Angelica137@users.noreply.github.com>
2022-04-07 20:21:08 +0100

    test clean_ingredients(), red

---
+++ tests/test_sets.py
@@ -0,0 +1,8 @@
+from scripts.set import *
+
+
+def test_clean_ingredients():
+    ingredients = ['onions', 'tomatoes', 'ginger paste', 'garlic paste', 'ginger paste', 'vegetable oil', 'bay leaves', 'cloves', 'cardamom', 'cilantro',
+                   'peppercorns', 'cumin powder', 'chickpeas', 'coriander powder', 'red chili powder', 'ground turmeric', 'garam masala', 'chickpeas', 'ginger', 'cilantro']
+    assert clean_ingredients('Punjabi-Style Chole', ingredients) == ('Punjabi-Style Chole', {'garam masala', 'bay leaves', 'ground turmeric', 'ginger', 'garlic paste', 'peppercorns',
+                                                                                             'ginger paste', 'red chili powder', 'cardamom', 'chickpeas', 'cumin powder', 'vegetable oil', 'tomatoes', 'coriander powder', 'onions', 'cilantro', 'cloves'})
