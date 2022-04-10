7b35eb69e4b8c8fc1a3daa8714ac641e08d71eca
Angelica <Angelica137@users.noreply.github.com>
2022-04-10 20:18:58 +0100

    test categorise_dish() Omnivore, red

--- tests/test_sets.py
+++ tests/test_sets.py
@@ -22,8 +22,16 @@ def test_check_drink_alcohol():
         'Shirley Tonic', ingredients) == 'Shirley Tonic Cocktail'
 
 
-def test_categorise_dish():
+def test_categorise_dish_vegan():
     ingredients = ['tofu', 'soy sauce', 'salt', 'black pepper', 'cornstarch', 'vegetable oil',
                    'garlic', 'ginger', 'water', 'vegetable stock', 'lemon juice', 'lemon zest', 'sugar']
     assert categorise_dish("Sticky Lemon Tofu",
                            ingredients) == 'Sticky Lemon Tofu: VEGAN'
+
+
+def test_categorise_dish_omnivore():
+    ingredients = ['shrimp', 'bacon', 'avocado', 'chickpeas', 'fresh tortillas', 'sea salt',
+                   'guajillo chile', 'slivered almonds', 'olive oil', 'butter', 'black pepper', 'garlic', 'onion']
+    dish_name = 'Shrimp Bacon and Crispy Chickpea Tacos with Salsa de Guacamole'
+    assert categorise_dish(
+        dish_name, ingredients) == 'Shrimp Bacon and Crispy Chickpea Tacos with Salsa de Guacamole: OMNIVORE'
